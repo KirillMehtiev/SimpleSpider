afaa0d9a3cfa3b5ccd741ba286d6467d708d2db6
Kirill Mehtiev <kirill.mehtiev@gmail.com>
2017-05-11 19:29:14 +0300

    Moved comment from implementation to abstraction

--- src/bll/Abstractions/IUrlFilter.cs
+++ src/bll/Abstractions/IUrlFilter.cs
@@ -3,8 +3,17 @@ using System.Collections.Generic;
 
 namespace BLL.Abstractions
 {
+    /// <summary>
+    /// Helper for filtering URLs
+    /// </summary>
     public interface IUrlFilter
     {
+        /// <summary>
+        /// Remove all URLs which do not refer to current website pages
+        /// </summary>
+        /// <param name="urls">URLs to be filtered</param>
+        /// <param name="baseUri">Base URL for URLs</param>
+        /// <returns>Filtered URLs which refer to website's pages</returns>
         ICollection<Uri> RemoveUnnecessary(ICollection<string> urls, Uri baseUri);
     }
 }
--- src/bll/Implementation/UrlFilter.cs
+++ src/bll/Implementation/UrlFilter.cs
@@ -5,17 +5,8 @@ using System.Linq;
 
 namespace BLL.Implementation
 {
-    /// <summary>
-    /// Helper for filtering URLs
-    /// </summary>
     public class UrlFilter : IUrlFilter
     {
-        /// <summary>
-        /// Remove all URLs which do not refer to current website pages
-        /// </summary>
-        /// <param name="urls">URLs to be filtered</param>
-        /// <param name="baseUri">Base URL for URLs</param>
-        /// <returns>Filtered URLs which refer to website's pages</returns>
         public ICollection<Uri> RemoveUnnecessary(ICollection<string> urls, Uri baseUri)
         {
             var result = new List<Uri>();
